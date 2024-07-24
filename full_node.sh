9ec60e82cfc1c7fe83f418a79dd4d68abd7462ff
jimao <shihao.gu@perfma.com>
2024-07-24 12:34:49 +0800

    shell脚本

--- full_node.sh
+++ full_node.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd build/bin
 ./geth --version
+./geth \
+  --sepolia \
+  --syncmode snap \
+  --datadir ./sepolia_node \
+  --beacon.api https://sepolia.lightclient.xyz \
+  --beacon.checkpoint 0xd1f4d00ab92951aacc7caa6581b7829895670b4abdcfc21ba21855c148043689
